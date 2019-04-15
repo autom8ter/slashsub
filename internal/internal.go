57adf09b7a60b4b9502c0441c85b9ce7bccbcb11
autom8ter <admin@autom8ter.com>
2019-04-15 10:38:19 -0600

    validation

--- internal/internal.go
+++ internal/internal.go
@@ -1,14 +1,23 @@
 package internal
 
 import (
+	"bytes"
 	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"github.com/autom8ter/api/go/api"
 	"github.com/autom8ter/gosub"
 	"github.com/autom8ter/gosub/driver"
 	"github.com/nlopes/slack"
+	"io/ioutil"
+	"math"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var SLASH_FUNCTION_URL = "https://us-central1-autom8ter-19.cloudfunctions.net/SlashFunction"
@@ -16,6 +25,7 @@ var PROJECT_ID = os.Getenv("PROJECT_ID")
 var SLACK_SIGNING_SECRET = []byte(os.Getenv("SLACK_SIGNING_SECRET"))
 
 type SlashSub struct {
+	secret []byte
 	pubsub *driver.Client
 }
 
@@ -26,6 +36,7 @@ func New(service string, middlewares ...driver.Middleware) (*SlashSub, error) {
 	}
 
 	s := &SlashSub{
+		secret: SLACK_SIGNING_SECRET,
 		pubsub: &driver.Client{
 			ServiceName: service,
 			Provider:    provider,
@@ -42,6 +53,7 @@ func (s *SlashSub) Client() *driver.Client {
 }
 
 func (s *SlashSub) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.ValidateRequest(r)
 	handler()(w, r)
 }
 
@@ -89,4 +101,52 @@ func handler() http.HandlerFunc {
 		}
 		fmt.Fprintf(w, "%s\n", "command parsed and sent to backend for processing ✔")
 	}
+}
+
+func  (s *SlashSub) ValidateRequest(r *http.Request) bool {
+	timestamp := r.Header["X-Slack-Request-Timestamp"][0]
+
+	// Verify the timestamp is less than 5 minutes old, to avoid replay attacks.
+	now := time.Now().Unix()
+	messageTime, err := strconv.ParseInt(timestamp, 0, 64)
+	if err != nil {
+		api.Util.Entry().Errorln("[ERROR] Invalid timestamp:", timestamp)
+		return false
+	}
+	if math.Abs(float64(now-messageTime)) > 5*60 {
+		api.Util.Entry().Errorln("[ERROR] Timestamp is from > 5 minutes from now")
+		return false
+	}
+
+	// Get the signature and signing version from the HTTP header.
+	parts := strings.Split(r.Header["X-Slack-Signature"][0], "=")
+	if parts[0] != "v0" {
+		api.Util.Entry().Errorln("[ERROR] Unsupported signing version:", parts[0])
+		return false
+	}
+	signature, err := hex.DecodeString(parts[1])
+	if err != nil {
+		api.Util.Entry().Errorln("[ERROR] Invalid message signature:", parts[1])
+		return false
+	}
+
+	// Read the request body.
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		api.Util.Entry().Errorln("[ERROR] Can't read request body:", err)
+		return false
+	}
+
+	// Generate the HMAC hash.
+	prefix := fmt.Sprintf("v0:%v:", timestamp)
+
+	hash := hmac.New(sha256.New, s.secret)
+	hash.Write([]byte(prefix))
+	hash.Write(body)
+
+	// Reset the request body so it can be read again later.
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+	// Verify our hash matches the signature.
+	return hmac.Equal(hash.Sum(nil), []byte(signature))
 }
